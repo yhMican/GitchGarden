c3c1d5286ca0ae3826dd7057cc0694f446f079f2
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-19 11:33:46 -0700

    up to lec 135

---
+++ Glitch Garden/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+		Debug.Log ("Don't destroy on load: " + name);
+	}
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> (); // necessary because this script is a different component from Audio Source
+	}
+
+	void OnLevelWasLoaded (int level) {
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		Debug.Log ("Playing clip: " + thisLevelMusic);
+
+		if (thisLevelMusic) {
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+	
+}
